87299353bbb9caa5ace43a12821d9505a3d69d4c
Luiz Felipe de Souza <luiz.f.souza@live.com>
2022-05-16 20:21:42 -0300

    theme color setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,17 @@
-function App() {
-  return <h1 className="text-2xl text-red-500">Hello, world</h1>;
-}
+import { useState } from "react";
+import { SunIcon } from "./components/Icons";
+
+const App = () => {
+  return (
+    <main className="">
+      <header className="bg-veryDarkGrayishBlue">
+        <div className="flex items-center justify-between">
+          <h1 className="text-2xl text-white">TODO</h1>
+          <SunIcon />
+        </div>
+      </header>
+    </main>
+  );
+};
 
 export default App;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brightBlue: "var(--color-base-brightBlue)",
+        checkBackgroundLeft: "#999999",
+        checkBackgroundRight: "#919191",
+        //light theme
+        veryLightGray: "var(--color-veryLightGray)",
+        veryLightGrayishBlue: "var(--color-veryLightGrayishBlue)",
+        lightGrayishBlue: "var(--color-lightGrayishBlue)",
+        darkGrayishBlue: "var(--color-darkGrayishBlue)",
+        veryDarkGrayishBlue: "var(--color-veryDarkGrayishBlue)",
+        veryDarkDesaturatedBlue: "var(--color-veryDarkDesaturatedBlue)",
+      },
+    },
   },
   plugins: [],
 };
